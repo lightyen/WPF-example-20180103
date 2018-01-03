ef42546af38d7c0e3caee2fa8a69daa4e05296b9
lightyen <lttlightyen@gmail.com>
2018-01-03 09:41:36 +0800

    Test

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        Window1 myWindow;
+        
+        public List<Daddy> Data
+        {
+            get; set;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myWindow = new Window1();
+
+            Data = new List<Daddy>
+            {
+                new Daddy { Name = "John", Money = 835593933832324142M },
+                new Daddy { Name = "Alisa", Money = 7120242M },
+            };
+            this.DataContext = this;
+        }
+
+        public void MyDataGridSelectRow(int index)
+        {
+            if (index >= 0 && index < MyDataGrid.Items.Count)
+            {
+                var item = MyDataGrid.Items[index];
+                MyDataGrid.SelectedItem = item;
+            }
+        }
+
+        private void HeHeHe_Click(object sender, RoutedEventArgs e)
+        {
+            myWindow.Show();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            myWindow.WindowClosed = true;
+            myWindow.Close();
+        }
+    }
+
+    public class Daddy : INotifyPropertyChanged
+    {
+        string name;
+
+        Decimal money;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
+        }
+
+        public Decimal Money
+        {
+            get => money;
+            set
+            {
+                money = value;
+                NotifyPropertyChanged(nameof(Money));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class Window1 : Window
+    {
+        public bool WindowClosed { set; get; }
+
+        public Window1()
+        {
+            InitializeComponent();
+
+            WindowClosed = false;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var mainWin = App.Current.MainWindow as MainWindow;
+            try
+            {
+                int i = int.Parse(MyTextBox.Text);
+                mainWin.MyDataGridSelectRow(i);
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (!WindowClosed)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+    }
+}
