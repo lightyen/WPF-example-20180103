8f5c00ff2feb7a3337f1fda80525f526beb9a3a3
lightyen <lttlightyen@gmail.com>
2018-01-04 08:44:08 +0800

    ToggleButton

--- WpfApp1/Window1.xaml.cs
+++ WpfApp1/Window1.xaml.cs
@@ -1,15 +1,31 @@
-﻿using System.ComponentModel;
+﻿
 using System.Windows;
 using System.Windows.Controls;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace WpfApp1
 {
-    public partial class Window1 : Window
+    public partial class Window1 : Window, INotifyPropertyChanged
     {
         public bool WindowClosed { set; get; }
 
+        private bool enabled;
+
+        public bool Enabled
+        {
+            get => enabled;
+            set
+            {
+                enabled = value;
+                NotifyPropertyChanged(nameof(Enabled));
+            }
+        }
+
         public Window1()
         {
+            this.DataContext = this;
+
             InitializeComponent();
 
             WindowClosed = false;
@@ -17,17 +33,19 @@ namespace WpfApp1
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            var mainWin = App.Current.MainWindow as MainWindow;
-            try
+            if (enabled)
             {
-                int i = int.Parse(MyTextBox.Text);
-                mainWin.MyDataGridSelectRow(i);
-            }
-            catch
-            {
+                var mainWin = App.Current.MainWindow as MainWindow;
+                try
+                {
+                    int i = int.Parse(MyTextBox.Text);
+                    mainWin.MyDataGridSelectRow(i);
+                }
+                catch
+                {
 
+                }
             }
-            
         }
 
         protected override void OnClosing(CancelEventArgs e)
@@ -38,5 +56,24 @@ namespace WpfApp1
                 Hide();
             }
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Enabled = true;
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Enabled = false;
+        }
+
+        
     }
 }
